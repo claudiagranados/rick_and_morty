ee49e892245175a61717cff3cac4331bd5ca496b
claudiagranados <claudiagranados@outlook.com>
2023-09-22 18:30:38 -0500

    M4 sin CSS

--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -1,16 +1,21 @@
-const users = require('../utils/users');
-
-const login = (req, res) => {
-    const {email, password } = req.query;
-
-    const userFound = users.find((user) => user.email === email && user.password === password)
-
-    userFound
-    ? res.status(200).json({ access: true })
-    : res.status(404).json({ access: false })
-}
+const {User} = require ('../DB_connection');
 
 
-module.exports = {
-    login
+module.exports = async (req, res)=>{
+    try {
+        const { email, password} = req.query;
+
+        if(!email || !password) return res.status(400).send("Faltan datos");
+
+        const user = await User.findOne({where: {email}})
+
+        if(!user) return res.status(404).send("Usuario no encontrado");
+
+        return user.password === password 
+        ? res.json({access: true})
+        : res.status(403).send("Contraseña incorrecta");
+
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
 };
